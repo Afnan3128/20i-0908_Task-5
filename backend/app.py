0e6963117f45449e936ff2916a9d05d51259ae22
Afnan3128 <i200908@nu.edu.pk>
2024-03-27 23:28:27 +0500

    Initial Commit

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+# backend/app.py
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient("mongodb://mongo:27017/")
+db = client["mydatabase"]
+
+# Define route to handle root URL
+@app.route('/')
+def index():
+    return 'Hello, this is the backend server!'
+
+# Define route to handle favicon request
+@app.route('/favicon.ico')
+def favicon():
+    return ''
+
+# Define route to handle storing data
+@app.route('/store', methods=['POST'])
+def store_data():
+    data = request.json
+    db.my_collection.insert_one(data)
+    return jsonify({"message": "Data stored successfully"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
